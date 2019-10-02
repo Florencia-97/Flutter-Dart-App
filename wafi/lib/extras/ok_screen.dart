5ee7508e687a3d14487fe6b125a33c19cb290f59
Florencia-97 <florrr1997@gmail.com>
2019-10-02 00:14:50 -0300

    Adding animation to ok screen

--- wafi/lib/extras/ok_screen.dart
+++ wafi/lib/extras/ok_screen.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class OkScreen extends StatefulWidget {
     @override
     _OkScreenState createState() => _OkScreenState();
 }
 
-class _OkScreenState extends State<OkScreen>{
+class _OkScreenState extends State<OkScreen> with SingleTickerProviderStateMixin{
+
+    static const FACE_LEFT_ANGLE = -pi / 2;
+    static const FACE_RIGHT_ANGLE = pi / 2;
 
     Animation animation;
     AnimationController animationController;
+
+    double _angle =  FACE_RIGHT_ANGLE;
+
+    @override
+    void initState() {
+      super.initState();
+
+      animationController = AnimationController(
+        duration: Duration(milliseconds: 2000), vsync: this)
+        ..addListener(() {
+        this.setState(() {});
+      })
+      ..addStatusListener((status){
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+        _angle = FACE_LEFT_ANGLE;
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+        _angle = FACE_RIGHT_ANGLE;
+      }
+      });
+
+      Tween _tween = new AlignmentTween(
+        begin: Alignment(-0.7, 0.0),
+        end: Alignment(0.7, 0.0),
+      );
+
+      animation = _tween.animate(animationController);
+      animationController.forward();
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -18,32 +52,47 @@ class _OkScreenState extends State<OkScreen>{
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              RotationTransition(
-                turns: AlwaysStoppedAnimation(-35/360),
-                child: Icon(Icons.send,
-                color: Colors.white,
-                size: 120.0,
+              Container(
+                alignment: animation.value,
+                child: Transform.rotate(
+                  angle: _angle,
+                  child:  RotationTransition(
+                    turns: AlwaysStoppedAnimation(-90/360),
+                    child: Icon(Icons.send,
+                    color: Colors.white,
+                    size: 100.0,
+                    )
+                  ),
                 )
               ),
-              Center(child: Padding(
-                  padding: EdgeInsets.only(left: 50.0, right: 50.0, top: 35.0, bottom: 20.0),
-                  child: Text('Tu pedido se realizó correctamente!',
+              Center(
+                child: Container(
+                padding: EdgeInsets.only(left: 50.0, right: 50.0, top: 35.0, bottom: 20.0),
+                child: Text(
+                  'Tu pedido se realizó correctamente!',
                   style: TextStyle(fontSize: 22, color: Colors.white),
                   ),
-                ),),
+                ),
+              ),
               RaisedButton(
                 onPressed: () {Navigator.pop(context);},
-                child: Padding(
+                child: Container(
                   padding: EdgeInsets.only(left: 50.0, right: 50.0, top: 10.0, bottom: 10.0),
                   child: Text('Ir a mis pedidos',
                     style: TextStyle(fontSize: 22, color: Colors.teal),
                   ),
                 ),
-              color: Colors.white,
+                color: Colors.white,
               )
             ],
           ),
         ),
       );
+    }
+
+    @override
+    void dispose() {
+      animationController.dispose();
+      super.dispose();
     }
 }
